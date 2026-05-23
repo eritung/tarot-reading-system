eb6c293728c91020c66b5342376560bd13ac2ee5
Eri <eritung0421@gmail.com>
2026-05-23 16:38:38 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const SUPABASE_URL = "https://gaixgfywxignacbgxrvk.supabase.co"
-export const SUPABASE_ANON_KEY = "sb_publishable_rTmsY0tCQp_6Wt0Wu5GgEw_3EefJV6i"
+export const SUPABASE_URL = "https://smrvmsatetjfvdhxaycy.supabase.co"
+export const SUPABASE_ANON_KEY = ".sb_publishable__vrIq0U0AgwcqjbguxWA_w_zYDEJzAY"
 export const FUNCTION_URL = `${SUPABASE_URL}/functions/v1/tarot-reading`
+
